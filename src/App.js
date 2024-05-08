310bcc56d1aa4f62e78f4a758725f4336a3e2021
supanut klakerdpol <supanut.kl@ku.th>
2024-05-08 18:07:49 +0700

    make style in header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './component/Header';
-import Footer from './component/Footer';
-import Body from './component/Body';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./component/Header";
+import Footer from "./component/Footer";
+import Body from "./component/Body";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Body/>
-      <Footer/>
+      <div className="content">
+        <Header />
+        <Body />
+        <Footer />
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import global_en from "./translations/en/global.json";
+import global_th from "./translations/th/global.json";
+import i18next from "i18next";
+import { I18nextProvider } from "react-i18next";
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "en",
+  resources: {
+    en: { global: global_en },
+    th: { global: global_th },
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
 
